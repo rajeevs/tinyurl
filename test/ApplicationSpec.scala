0dfd5f2fc3928f37805d84a20375b6a717c89a22
Rajeev Sudhakar <srajeev@gmail.com>
2013-11-14 04:47:29 -0800

    Making a note that tests are Todo

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,6 +5,8 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
+/* Not implemented yet : Rajeev Sudhakar*/
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
